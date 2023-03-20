4560d6127f2edc10c6a9bf32bebd1b55dd9be9ea
Akonil <akonilh2o@proton.me>
2023-03-20 22:27:32 +0330

    fix: update social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/hiddify-social-card.jpg",
       navbar: {
         title: "هیدیفای",
         logo: {
